cec3fdfe1b01b5a4ac6fe60343405f7a556048d3
Ahmet <Ahmet@DESKTOP-V98IOMF>
2025-12-28 20:46:11 +0300

    First

--- obj/Debug/net8.0/DockerNginx.AssemblyInfo.cs
+++ obj/Debug/net8.0/DockerNginx.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DockerNginx")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8f3cc687302ba495ff37f0583d5bd4018ea4c7da")]
 [assembly: System.Reflection.AssemblyProductAttribute("DockerNginx")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DockerNginx")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
